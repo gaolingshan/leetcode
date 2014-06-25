d09f319077156004026546bea3ee0c7e388b755b
flashhack <flashhack2@gmail.com>
2014-06-25 15:22:10 -0400

    Revert "2/151"
    This reverts commit eeb1a16e2c5f84c2cb2bc89ae57f6b73e6c3adce.

--- Median of Two Sorted Arrays/Source.cpp
+++ Median of Two Sorted Arrays/Source.cpp
@@ -5,34 +5,24 @@ using namespace std;
 
 class Solution {
 public:
-	int findkth(int A[], int Alen, int B[], int Blen, int k)
+	int BinarySearch(int A[], int start, int from, int data)
 	{
-		if(Alen==0) return B[k-1];
-		if(Blen==0) return A[k-1];
-		if(k==1) return A[0]<B[0]?A[0]:B[0];
-		int Amid= (double)k*Alen/(Alen+Blen)-1;
-		int Bmid = k-Amid-2;
-		if(A[Amid] > B[Bmid]) return findkth(A,Amid+1,&B[Bmid+1],Blen-Bmid-1,k-Bmid-1);
-		if(A[Amid] == B[Bmid]) return A[Amid];
-		if(A[Amid] < B[Bmid]) return findkth(&A[Amid+1],Alen-Amid-1,B,Bmid+1,k-Amid-1);
+		int i=start, j=from;
+		while(i<j)
+		{
+			int mid = (i + j) / 2;
+			if(data <= A[mid]) j=mid-1;
+			if(data > A[mid]) i=mid+1;
+		}
+		return i;
 	}
 
 	double findMedianSortedArrays(int A[], int m, int B[], int n) {
-		if((m+n)%2 == 0)
-			return ((double)findkth(A,m,B,n,(m+n)/2) + (double)findkth(A,m,B,n,(m+n)/2+1))/2;
-		else
-			return findkth(A,m,B,n,(m+n)/2+1);
+		
 	}
 };
 
 int main()
 {
-	Solution *s = new Solution();
-	int A[] = {4,5,6,7};
-	int B[] = {1,2,10};
-	//cout << s->findkth(A,3,B,3,6) << endl;
-	cout << s->findMedianSortedArrays(A,4,B,3) << endl;
-	//cout << (int)((double)2*1/3-1) << endl;
-	system("pause");
 	return 0;
 }
