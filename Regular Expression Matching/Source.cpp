aa738e0f22a6aa696b6a1589c2f4c4024eee1c69
flashhack <flashhack2@gmail.com>
2014-07-08 18:44:51 -0400

    10/151

--- Regular Expression Matching/Source.cpp
+++ Regular Expression Matching/Source.cpp
@@ -7,16 +7,65 @@ using namespace std;
 
 class Solution {
 public:
+	int f[1000][1000];
 	bool isMatch(const char *s, const char *p) {
+		int i=strlen(s),j=strlen(p);
+		if(f[i][j] != 0) return (f[i][j]==1?true:false);
+		if(*s==0 && *p==0) 
+		{
+			f[i][j] = 1;
+			return true;
+		}
+		if((*s!=0 && *p==0))
+		{
+			f[i][j] = -1;
+			return false;
+		}
 		
+		int tmp = strlen(s);
+
+		if(*(p+1)=='*')
+		{
+			if(*s != 0)
+			{
+				if(*s == *p || *p=='.') 
+				{
+					if(isMatch(s+1,p) || isMatch(s+1,p+2) || isMatch(s,p+2)) f[i][j]=1; else f[i][j]=-1;
+					return (f[i][j]==1?true:false);
+				}
+			}
+			if(isMatch(s,p+2)) f[i][j]=1; else f[i][j]=-1;
+			return (f[i][j]==1?true:false);
+		}
+		if(*s != 0 && (*s == *p || *p=='.')) 
+		{
+			if(isMatch(s+1,p+1)) f[i][j]=1; else f[i][j]=-1;
+			return (f[i][j]==1?true:false);
+		}
+		
+		else 
+		{
+			f[i][j]=-1;
+			return false;
+		}
 	}
 };
 
 int main()
 {
 	Solution *s = new Solution();
+	//cout << s->isMatch("",".") << endl;			//0
+	//cout << s->isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c") << endl;		//0
+	//cout << s->isMatch("ba", ".*a*a") << endl;		//1
+	//cout << s->isMatch("a", "ab*") << endl;			//1
+	//cout << s->isMatch("aaa", "a*a") << endl;		//1
 
-	//cout << s->isPalindrome(-2147447418) << endl;
+	//cout << s->isMatch("aab", "c*a*b") << endl;		//1
+	//cout << s->isMatch("aaaaa", "a*") << endl;		//1
+	//cout << s->isMatch("dd", ".*abc") << endl;		//0
+	//cout << s->isMatch("a",".*") << endl;			//1
+	//cout << s->isMatch("",".*") << endl;			//1
+	//cout << s->isMatch("a","") << endl;				//0
 	system("pause");
 	return 0;
 }
