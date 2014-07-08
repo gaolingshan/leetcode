2fd6133b5cc40776cba6e6f4913dbf76fffa0add
flashhack <flashhack2@gmail.com>
2014-07-08 18:50:23 -0400

    10_modified/151

--- Regular Expression Matching/Source.cpp
+++ Regular Expression Matching/Source.cpp
@@ -7,47 +7,21 @@ using namespace std;
 
 class Solution {
 public:
-	int f[1000][1000];
 	bool isMatch(const char *s, const char *p) {
-		int i=strlen(s),j=strlen(p);
-		if(f[i][j] != 0) return (f[i][j]==1?true:false);
-		if(*s==0 && *p==0) 
-		{
-			f[i][j] = 1;
-			return true;
-		}
-		if((*s!=0 && *p==0))
-		{
-			f[i][j] = -1;
-			return false;
-		}
-		
-		int tmp = strlen(s);
+		if(*s==0 && *p==0) 			return true;
+		if(*s!=0 && *p==0)			return false;
 
 		if(*(p+1)=='*')
 		{
-			if(*s != 0)
+			while(*s == *p || *p=='.' && *s != 0)
 			{
-				if(*s == *p || *p=='.') 
-				{
-					if(isMatch(s+1,p) || isMatch(s+1,p+2) || isMatch(s,p+2)) f[i][j]=1; else f[i][j]=-1;
-					return (f[i][j]==1?true:false);
-				}
+				if(isMatch(s,p+2)) return true;
+				s++;
 			}
-			if(isMatch(s,p+2)) f[i][j]=1; else f[i][j]=-1;
-			return (f[i][j]==1?true:false);
+			return isMatch(s,p+2);
 		}
-		if(*s != 0 && (*s == *p || *p=='.')) 
-		{
-			if(isMatch(s+1,p+1)) f[i][j]=1; else f[i][j]=-1;
-			return (f[i][j]==1?true:false);
-		}
-		
-		else 
-		{
-			f[i][j]=-1;
-			return false;
-		}
+		if(*s != 0 && (*s == *p || *p=='.')) return isMatch(s+1,p+1);
+		else return false;
 	}
 };
 
